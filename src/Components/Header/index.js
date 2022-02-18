b1ea551f7b6c480824af4edd93018c660748b5b3
MJ <m3hm3tcan@gmail.com>
2022-02-18 22:07:00 +0100

    clean up

---
+++ src/Components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <div className="App">
+        <p>TEST</p>
+      </div>
+    );
+  }
+  
+export default Header;
---
+++ src/Components/Search/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+      <div className="App">
+        <p>TEST</p>
+      </div>
+    );
+  }
+  
+export default Search;

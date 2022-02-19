2f1f09eced4a061e32faf8f8a5496f3561bb626b
MJ <m3hm3tcan@gmail.com>
2022-02-19 17:19:16 +0100

    get user repo list with error control

---
+++ src/Components/RepoList/RepoItem.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const RepoItem = ({ data }) => {
+    return (
+        <>
+            {data.data.user.repositories !== undefined && data.data.user.repositories.nodes.map((repo,i) => (
+
+                <li className="flex flex-row pt-1 pb-4 w-600" key={i}>
+                    <div
+                        className="select-none flex flex-1 items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-2 rounded-2xl border-2 p-6 hover:shadow-2xl border-red-400">
+                        <div className="flex-1 pl-1 mr-16">
+                            <div className="font-medium  w-px-100">
+                                {repo.name}
+                            </div>
+                            <div className="font-small  w-px-100">
+                                {repo.description}
+                            </div>
+                            <div className="font-small">
+                               {`is Fork: ${repo.isFork}`}
+                            </div>
+                            <div className="font-small">
+                               {`Total issues : ${repo.issues.totalCount}`}
+                            </div>
+                        </div>
+                        <a
+                            className="w-1/4 text-wrap text-center flex text-white text-bold flex-col rounded-md bg-red-500 justify-center items-center mr-2 p-1"
+                            href={repo.url} target="_blank">
+                            Open in GitHub
+                        </a>
+                    </div>
+                </li>
+
+            ))
+            }
+        </>
+
+    )
+}
+
+export default RepoItem;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import Search from './../../Components/Search'
 import { getCurrentUser, getUserRepoList } from './../../Services/DataService'
-// import NotFound from '../../Components/NotFound';
-// import UserList from './../../Components/UserList'
+import RepoList from './../../Components/RepoList'
+import Loading from './../../Components/Loading'
 
 const Home = () => {
   const [currentUser, setCurrentUser] = useState()
-  const [searchResult, setSearchResult] = useState()
+  const [searchResult, setSearchResult] = useState(null)
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(()=>{
     const fecthUser = async () =>{
       let data={};
       const getUser = await getCurrentUser().then(res=>{ 
         data= res.data.data.viewer;
-      });
+      }).catch((error) => console.log('error', error))
+      
       setTimeout(getUser, 1000)
       setCurrentUser(data)
     } 
@@ -22,7 +24,14 @@ const Home = () => {
 
   const handleSubmit = async (e, userName) => {
     e.preventDefault()
-    await getUserRepoList(userName).then(res => setSearchResult(res.data))
+    setIsLoading(true)
+    await getUserRepoList(userName).then(res => {
+      setSearchResult(res.data)
+    }).catch(error=>{
+      console.log('error', error)
+    }).finally(()=>{
+      setIsLoading(false)
+    });
   }
 
   useEffect(()=>{
@@ -34,9 +43,9 @@ const Home = () => {
     <div className='items-center min-h-screen from-cyan-100 via-pink-200 to-red-200 bg-gradient-to-br pt-24' >
       <Search callBack={handleSubmit} />
 
-      {/* <NotFound/> */}
-      {/* <UserList /> */}
-
+      {isLoading && <Loading/> }
+      {!isLoading && searchResult !== null  && <RepoList data={searchResult}/> }
+      
       
     </div>
   );
